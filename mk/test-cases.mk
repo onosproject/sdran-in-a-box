c052287eb097e5bd84eb060bf759ace42b9b06db
Woojoong Kim <woojoong.m.kim@gmail.com>
2021-10-29 14:22:30 -0700

    Add rsm data plane test target

--- mk/test-cases.mk
+++ mk/test-cases.mk
@@ -39,3 +39,11 @@ test-e2-connection: | $(M)/ric
 test-e2-subscription: | $(M)/ric
 	@echo "*** Get E2 subscriptions through CLI ***"; \
 	kubectl exec -it deployment/onos-cli -n riab -- onos e2t get subscriptions;
+
+test-rsm-dataplane: $(M)/ric $(M)/omec $(M)/oai-ue
+	@echo "*** Test downlink traffic (UDP) ***"
+	sudo apt install -y iperf3
+	kubectl exec -it router -- apt install -y iperf3
+	iperf3 -s -B $(ip a show oaitun_ue1 | grep inet | grep -v inet6 | awk '{print $2}' | awk -F '/' '{print $1}') -p 5001 > /dev/null &
+	kubectl exec -it router -- iperf3 -u -c $(ip a show oaitun_ue1 | grep inet | grep -v inet6 | awk '{print $2}' | awk -F '/' '{print $1}') -p 5001 -b 20M -l 1450 -O 2 -t 12 --get-server-output
+	pkill -9 -ef iperf3
