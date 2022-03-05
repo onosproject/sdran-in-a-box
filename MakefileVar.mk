de068726322bd934a95d038294de6093d489274e
Woojoong Kim <woojoong.m.kim@gmail.com>
2022-03-05 09:34:27 +0900

    Add RSM xApp for the latest RIC option

--- MakefileVar.mk
+++ MakefileVar.mk
@@ -32,7 +32,7 @@ DEFAULT_HELM_ARGS				:= --set import.ran-simulator.enabled=true --set import.ono
 HELM_ARGS						?= $(DEFAULT_HELM_ARGS)
 HELM_ARGS_RANSIM				?= --set import.ran-simulator.enabled=true --set import.onos-pci.enabled=true
 HELM_ARGS_OAI					?= --set import.onos-rsm.enabled=true
-HELM_ARGS_RIC					?= --set import.onos-pci.enabled=false
+HELM_ARGS_RIC					?= --set import.onos-pci.enabled=false --set import.onos-rsm.enabled=true
 HELM_ARGS_RIC_E2AP101			?= --set import.onos-pci.enabled=false --set import.onos-rsm.enabled=true
 HELM_ARGS_FBAH					?= --set import.fb-ah-xapp.enabled=true --set import.fb-ah-gui.enabled=true --set import.ah-eson-test-server.enabled=true --set import.ran-simulator.enabled=true --set import.fb-kpimon-xapp.enabled=true
 HELM_ARGS_MLB       	        ?= --set import.ran-simulator.enabled=true --set import.onos-pci.enabled=true --set import.onos-mlb.enabled=true --set ran-simulator.pci.modelName=three-cell-n-node-model --set ran-simulator.pci.metricName=three-cell-n-node-metrics
