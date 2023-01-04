970d64250d743f59b85c2922fdcdb6899a21badb
Woojoong Kim <woojoong.kim@intel.com>
2023-01-04 15:08:40 -0800

    update atomix and fix mme ip address parameter (#187)

--- mk/reset-clean.mk
+++ mk/reset-clean.mk
@@ -43,6 +43,7 @@ reset-atomix:
 	helm uninstall atomix-raft-storage -n kube-system || true
 	helm uninstall raft-storage-controller -n kube-system || true
 	helm uninstall cache-storage-controller -n kube-system || true
+	helm uninstall atomix-runtime -n kube-system || true
 	kubectl delete -f https://raw.githubusercontent.com/atomix/kubernetes-controller/0a9e82ef37df25cf567a4dbc18f35b2bb454bda1/deploy/atomix-controller.yaml || true
 	kubectl delete -f https://raw.githubusercontent.com/atomix/raft-storage-controller/668951dff14e339f3c71b489863cbca8ec326a96/deploy/raft-storage-controller.yaml || true
 	kubectl delete -f https://raw.githubusercontent.com/atomix/cache-storage-controller/85014c6216e3d8cdf22df09aab3d1f16852fc584/deploy/cache-storage-controller.yaml || true
