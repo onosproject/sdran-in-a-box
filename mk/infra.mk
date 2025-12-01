b144df4652164a143ea4fd8e502931bc88ee7846
yhm1620 <yhm1620@gamil.com>
2025-12-01 06:32:13 +0000

    Fix atomix helm repo

--- mk/infra.mk
+++ mk/infra.mk
@@ -54,7 +54,7 @@ $(M)/helm-ready: | $(M)/k8s-ready
 	helm repo add incubator $(HELM_INCUBATOR_URL)
 	helm repo add cord $(HELM_OPENCORD_URL)
 	helm repo add sdran $(HELM_SDRAN_URL)
-	helm repo add atomix https://charts.atomix.io
+	helm repo add atomix https://atomix.github.io/charts.atomix.io
 	helm repo add onos https://charts.onosproject.org
 	helm repo update
 	touch $@
